4fb97d44319cefd61c6f26060c884acdc073966e
hamdi ben abdallah <hamdi.benabd89@gmail.com>
2022-08-25 15:30:15 +0200

    API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ const [users, setusers] = useState([])
+ const [loading, setloading] = useState(true)
+  // // the old way
+  //   useEffect(() => {
+  //    const getUsers=()=>{
+  //     fetch('https://jsonplaceholder.typicode.com/users')
+  //     .then((res)=>res.json())
+  //     .then((data)=>setusers(data))
+  //    };
+  //   getUsers()
+  //   setloading(false)
+  //   console.log(users)
+
+  //   }, [])
+useEffect(() => {
+  const getUsers=async()=>{
+    const res=await axios.get('https://jsonplaceholder.typicode.com/users')
+    console.log(res)
+    setusers(res.data)
+  }
+  getUsers()
+  setloading(false)
+},[])
+
+
+      return (
+        
+      
+    
+    
+    <div className="App">{
+      loading?<h1> loading...</h1>:
+      users.map(el=><div>
+        <h1>{el.name}</h1>
+        <h2>{el.email}</h2>
+
+      </div>)
+    }
+    
+
     </div>
   );
 }
